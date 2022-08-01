e869c78c27f7ff32c34d8b49e148606a45b6b38a
petrero <petrero@gmail.com>
2022-08-01 16:29:33 +0300

    10.111(2) Form validations - Register vee-validate library globally using a plugin

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './assets/tailwind.css'
 import './assets/main.css'
 import { createPinia } from 'pinia'
+import VeeValidatePlugin from './includes/validation.js'
 import App from './App.vue'
 import { routes } from './routes.js'
 import { createRouter, createWebHistory } from 'vue-router'
@@ -14,4 +15,5 @@ const router = createRouter({
 createApp(App)
   .use(router)
   .use(createPinia())
+  .use(VeeValidatePlugin)
   .mount('#app')
