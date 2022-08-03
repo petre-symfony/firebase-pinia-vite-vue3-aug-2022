80122ad44c5b8274593beeed4d9d7838d83376dd
petrero <petrero@gmail.com>
2022-08-03 21:01:34 +0300

    10.111 Form validations - Validating age

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -18,7 +18,7 @@
 	const schema = reactive({
 		name: 'required|min:3|max:100|alpha_spaces',
 		email: 'required|min:3|max:100|email',
-		age: '',
+		age: 'required|min_value:18|max_value:100',
 		password: '',
 		confirm_password: '',
 		country: '',
@@ -125,9 +125,10 @@
 						<!-- Age -->
 						<div class="mb-3">
 							<label class="inline-block mb-2">Age</label>
-							<input type="number"
+							<vee-field type="number" name="age"
 								class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
 								duration-500 focus:outline-none focus:border-black rounded" />
+							<error-message class="text-red-600" name="age" />
 						</div>
 						<!-- Password -->
 						<div class="mb-3">
