0c3e4c01cb431d0af456791f7926ff85dedb0ccf
petrero <petrero@gmail.com>
2022-08-03 20:31:09 +0300

    10.106 Form validations - Use vee components vee-form and vee-field instead of form and input field

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -89,11 +89,11 @@
 						</button>
 					</form>
 					<!-- Registration Form -->
-					<form v-show="tab === 'register'">
+					<vee-form v-show="tab === 'register'">
 						<!-- Name -->
 						<div class="mb-3">
 							<label class="inline-block mb-2">Name</label>
-							<input type="text"
+							<vee-field type="text" name="name"
 								class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
 								duration-500 focus:outline-none focus:border-black rounded"
 								placeholder="Enter Name" />
@@ -151,7 +151,7 @@
 						>
 							Submit
 						</button>
-					</form>
+					</vee-form>
 				</div>
 			</div>
 		</div>
