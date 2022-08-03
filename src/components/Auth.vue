4ed43c94f3f3e95949f02552a923b0627f562a05
petrero <petrero@gmail.com>
2022-08-03 20:49:27 +0300

    10.109(1) Form validations - Alternative syntax to apply rules to the name input

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -110,6 +110,9 @@
 								class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
 								duration-500 focus:outline-none focus:border-black rounded"
 								placeholder="Enter Name"
+							 	:rule = "{
+									required: true
+							 	}"
 							/>
 							<error-message class="text-red-600" name="name" />
 						</div>
