aeeee92dbf862b07f1f703126839d7ff5b78ebd9
petrero <petrero@gmail.com>
2022-08-04 20:38:46 +0300

    11.134 Authentication - Connecting the User with their Data in the Firebase Cloud User is stored in two places in Firebase. In user authenticated account and in firestore database. To know which user in authenticated account pertains to which user in firestore we change the way user is created in firestore database so as the unique identifier of the user inserted in firestore database to be the same as uid property of the user inserted in authenticated account. The normal way is that this unique identifier is auto generated

--- src/stores/storeUserLoggedIn.js
+++ src/stores/storeUserLoggedIn.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia"
-import {createUserWithEmailAndPassword} from "firebase/auth";
-import {firebaseAuth, usersCollection} from "@/includes/firebase";
-import { addDoc } from "firebase/firestore";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {firebaseAuth, firebaseFirestore } from "@/includes/firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 export const useUserStore = defineStore('user', {
 	state: () => ({
@@ -9,17 +9,26 @@ export const useUserStore = defineStore('user', {
 	}),
 	actions: {
 		async register(values) {
-			await createUserWithEmailAndPassword(
+			const userCredentials = await createUserWithEmailAndPassword(
 				firebaseAuth,
 				values.email,
 				values.password
 			)
-			await addDoc(usersCollection, {
+
+			// Create an initial document to update.
+			const userUidDocRef = doc(firebaseFirestore, "users", userCredentials.user.uid)
+			await setDoc(userUidDocRef, {
 				name: values.name,
 				email: values.email,
 				age: values.age,
 				country: values.country
 			})
+
+			await updateProfile(userCredentials.user, {
+				displayName: values.name
+			})
+
+
 			this.userLoggedIn = true
 		}
 	}
