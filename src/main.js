137cafee19448f988cd40ffc534df6c808e84fd9
petrero <petrero@gmail.com>
2022-08-04 20:55:11 +0300

    11.135 Authentication - After the user registered an account they are automatically logged in to the system. The token is stored locally on the system. The firebase sdk handles that. we've also taken the time to create a state property. The problem we faced is that if the user refreshes the page then they are no longer authenticated into the app. Any data stored in the store will become lost if the user navigates away from our site. The userLoggedIn state property is set to false. This value is not what we want. We want to be able to log the user back if the user come back to the application. We can use the token generated by the firebase to help us solve this problem. Firebase will keep track of the token for us even if the page refreshes. We can use it to tell the vue application to set the userLoggedIn state to true if the user is logged in.   First we need to load firebase before to load vue. We can't check if the user is logged in if firebase hasn't been initialized. The second step is to check the token app to the vue application has been initialized. If the token is valid then will set the state property to true.   We'll take care of the first step in this lecture. We want to wait for the authentication service is ready. Behind the scenes firebase will make a request to the authentication service. It will check if the user is logged in. After receiving a response firebase will fire an event we can listen to. We want to listen for that event. It will tell us if the user is logged in.   The authentication object is how we are able to listen for the event. onStateChanged has an argument, that is the callback function to call when the event is emitted. Firebase will emit the event whenever the user authentication state changes. This includes when the user is logged out. It will run immediatelly after firebase is loaded on the page. Is guaranted to run at list once. This event makes it safe to place the vue inside of it. Because this event can run multiple times we want to be sure that our vue app is initialized only once

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import './assets/tailwind.css'
 import './assets/main.css'
 import { createPinia } from 'pinia'
 import VeeValidatePlugin from './includes/validation.js'
-import './includes/firebase.js'
+import { firebaseAuth } from "@/includes/firebase";
+
+'./includes/firebase.js'
 import App from './App.vue'
 import { routes } from './routes.js'
 import { createRouter, createWebHistory } from 'vue-router'
@@ -13,8 +15,15 @@ const router = createRouter({
   routes,
 })
 
-createApp(App)
-  .use(router)
-  .use(createPinia())
-  .use(VeeValidatePlugin)
-  .mount('#app')
+let app;
+
+firebaseAuth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App)
+      .use(router)
+      .use(createPinia())
+      .use(VeeValidatePlugin)
+      .mount('#app')
+  }
+})
+
