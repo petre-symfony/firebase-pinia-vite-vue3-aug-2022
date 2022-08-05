6d1ee8364db9efd8d25f38bdc13ed5fe401b5668
petrero <petrero@gmail.com>
2022-08-05 22:33:38 +0300

    12.143(1) Routing - We are going to look at how the history mode can be used to change how urls work in our application. Currently we don't have links to navigate around. If you want to test the router properly without links we need to switch the mode.   Back in the browser refresh the page. Eveything seems the same except for the url. In the address bar the view router library has added a hash to the url. Very misterious! Let's try testing if the /about path works. Before we do switch to the network panel. We want to check that the page is not refresh. It shouldn't because the vue router will prevent that. It should we be able to switch between pages without having to redownload the assets. If there're already logs in the network panel you can clear it by pressing the clear button. Without removing the hash add the word about to the url. The message inside the about component will appear. The network panel won't log anything new. This means that even know the url has changed the page didn't refresh. Instead the router is able to change the content without having to reload the assets internally.   We can view more information about the routes by switching to the vue developer tools. There's a section called timeline. On the timeline we'll be able to view events. The history will provide you with a log of paths that have been accessed in the application. If I were to press the backwards or forwards button on the browser new paths will be logged. This logging capability is one of the benefit of using the vue router. It integrates perfectly with the vue developer tools. All this information is available to help us debug the application. I could even click on each path to see more details. We won't be diving too deep into this. We'll be explore it in future lections.   There is another section called Routes. It will contain a list of routes registered in our app. It will tell you which route is currently active. You can click on a route to view the details. We won't find much information because we're keeping our routes simple. This tool is a great way to check if a route is registered successfully without moving around in your application.   This is the bases of routing. The minimum requirements of creating a route is to set the path and component. The router will take care of the rest for you.   Let's talk about the hash you find in the address bar. To better understand what this does I'm going to switch over to wikipedia. I am on the page for pizza but any wikipedia entry will do. Every wikipedia page has a list of contents. There's links to sections on the page. If you can click on any of this list items you're automatically jumped to that section of the page. This is performed with basic html. Javascript is not required to perform this trick. Creating this effect isn't hard to call off. The first step is to assign an id to an element. After assigning an id you can create a link that points to the element by using a hash. I'm going to scroll back to the list of contents. I'll inspect one of the links. In the developer tools we can clearly see that the each href attribute is set to the sections id.   Browser will interpret this diffrently than full http urls. Instead of reloading the page it will jump to the section. This is what the vue router is using because jumping to a section doesn't cause the page to refresh. The router will override this behaviour by swapping out components. You may be curious as to why the router opts to use the hashing system. This is because the hash system has been around longer than most api's. It's a system that even works in legacy browsers. If you're building an application that must work on older browsers than you may want this behaviour. The problem with using hashes is that they don't get indexed by most search engines. Another issue is that users don't commonly type hashes in their address bars.

--- src/main.js
+++ src/main.js
@@ -8,10 +8,10 @@ import { firebaseAuth } from "@/includes/firebase";
 './includes/firebase.js'
 import App from './App.vue'
 import { routes } from './routes.js'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
