c83d037bdb9759e0bd7e1c54d736a2b0f253ea79
petrero <petrero@gmail.com>
2022-08-09 07:50:04 +0300

    12.153 Routing - Redirecting after logging out   If the user is on the manage page when they are logging out they are redirected to home page, otherwise they stay on the same page.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,29 @@
 <script setup>
 	import { useAuthModalShow } from '@/stores'
 	import { useUserStore } from '@/stores/storeUserLoggedIn.js'
+	import { useRouter, useRoute } from 'vue-router'
 	/*
-		store
-	 */
+	router
+ */
+	const router = useRouter()
+	const route = useRoute()
+	/*
+		/*
+			store
+		 */
 	const storeAuthModalShow = useAuthModalShow()
 
 	/*
 		user pinia store
 	 */
 	const storeUser = useUserStore()
+
+	const signOut = () => {
+		storeUser.signOut()
+		if (route.name === 'manage') {
+			router.push({ name: "home" })
+		}
+	}
 </script>
 <template>
 	<!-- Header -->
@@ -39,7 +53,7 @@
 							<router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
 						</li>
 						<li>
-							<a class="px-2 text-white" href="#" @click.prevent="storeUser.signOut">Logout</a>
+							<a class="px-2 text-white" href="#" @click.prevent="signOut">Logout</a>
 						</li>
 					</template>
 				</ul>
